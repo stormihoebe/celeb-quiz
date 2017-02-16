cc065caa7602d703ff31cc472a329310373ef87f
Janek Brandt and Stormi Hoebelheinrich <janek@>
2017-02-16 10:41:28 -0800

    add form that asks questions to pair you with marvel celebrities, js used to display appropriate match

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(function(){
+  $("#quiz").submit(function(event){
+    event.preventDefault();
+    var color = $("#color").val();
+    var age = parseInt($("#age").val());
+    var number = parseInt($("#number").val());
 
+    $(".celeb").hide();
+
+    if (color === "#000000") {
+      $(".natasha").show();
+    } else if (age > 45 && number > 100 ) {
+      $(".tonyStark").show();
+      console.log("This part is working")
+    } else {
+      $(".natasha").show();
+
+    };
+
+  });
 
 });
